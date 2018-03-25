7c2e7ad69267b445f1f39d124ff8e91f15b5d00d
Belood <alexandre.ung96@gmail.com>
2018-03-25 16:42:51 -0400

    Merge conflict resolved

--- src/src/Optimisation/SettingValue.java
+++ src/src/Optimisation/SettingValue.java
@@ -5,29 +5,28 @@ package Optimisation;
  */
 public class SettingValue {
 
-    public static int MIN_FLOW = 0;
-   // public static int MAX_FLOW = 180;
-   public static int MAX_FLOW = 400; //test version
+	public static int MIN_FLOW = 0;
+	public static int MAX_FLOW = 555;
 
-    public static int MIN_FLOW_T1 = 0;
-    public static int MAX_FLOW_T1 = 180;
+	public static int MIN_FLOW_T1 = 0;
+	public static int MAX_FLOW_T1 = 180;
 
-    public static int MIN_FLOW_T2 = 0;
-    public static int MAX_FLOW_T2 = 180;
+	public static int MIN_FLOW_T2 = 0;
+	public static int MAX_FLOW_T2 = 180;
 
-    public static int MIN_FLOW_T3 = 0;
-    public static int MAX_FLOW_T3 = 180;
+	public static int MIN_FLOW_T3 = 0;
+	public static int MAX_FLOW_T3 = 180;
 
-    public static int MIN_FLOW_T4 = 0;
-    public static int MAX_FLOW_T4 = 180;
+	public static int MIN_FLOW_T4 = 0;
+	public static int MAX_FLOW_T4 = 180;
 
-    public static int MIN_FLOW_T5 = 0;
-    public static int MAX_FLOW_T5 = 180;
+	public static int MIN_FLOW_T5 = 0;
+	public static int MAX_FLOW_T5 = 180;
 
-    public static int DISCRETISATION = 5;
+	public static int DISCRETISATION = 5;
 
-    public static int NB_TURBINE = 5;
+	public static int NB_TURBINE = 5;
 
-
+	public static int NB_LINE = SettingValue.MAX_FLOW / SettingValue.DISCRETISATION;
 
 }
