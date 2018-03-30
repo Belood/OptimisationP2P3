38aebb48dc5a6ab50d20bd87badb7bef0565157f
Belood <alexandre.ung96@gmail.com>
2018-03-30 14:11:17 -0400

    Début interface graphique

---
+++ src/src/ui/IHM.java
@@ -0,0 +1,22 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class IHM extends Application {
+	public static void main(String[] args) {
+		launch(IHM.class,args);
+		}
+    public void start(Stage stage) throws Exception {
+       Parent root = FXMLLoader.load(getClass().getResource("IHM.fxml"));
+    
+        Scene scene = new Scene(root, 1280, 800);
+    
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
